a3cec7e198404b3c26a11d01668d023fc7929fcf
amponsemmichael <amponsem_michael@yahoo.com>
2024-09-02 21:28:39 +0000

    entity mapping

--- src/main/java/com/amponsem/model/Image.java
+++ src/main/java/com/amponsem/model/Image.java
@@ -1,12 +1,30 @@
 package com.amponsem.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.sql.Blob;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fileName;
     private String fileType;
 
+    @Lob
     private Blob image;
     public String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
 }
